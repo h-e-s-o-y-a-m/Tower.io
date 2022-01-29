0696cbd2145a72812cd0f561b9d276b72c9fdf5f
xStayOuTx <people.can.not.fly@gmail.com>
2022-01-29 22:12:20 +0300

    Add "Restart" button

--- Assets/Scripts/ExplodeCubes.cs
+++ Assets/Scripts/ExplodeCubes.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class ExplodeCubes : MonoBehaviour {
 
+    public GameObject restartButton;
     private bool _collisionSet;
 
     private void OnCollisionEnter(Collision collision) {
@@ -12,6 +13,8 @@ public class ExplodeCubes : MonoBehaviour {
                 child.gameObject.GetComponent<Rigidbody>().AddExplosionForce(70f, Vector3.up, 5f);
                 child.SetParent(null);
             }
+            restartButton.SetActive(true);
+            Camera.main.transform.localPosition -= new Vector3(0, 0, 3f);
             Destroy(collision.gameObject);
             _collisionSet = true;
         }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -12,6 +12,8 @@ public class GameController : MonoBehaviour {
     public GameObject cubeToCreate, allCubes;
     private Rigidbody allCubesRb;
 
+    private bool IsLose;
+
     private List<Vector3> allCubesPositions = new List<Vector3>{
         new Vector3(0, 0, 0),
         new Vector3(1, 0, 0),
@@ -25,13 +27,15 @@ public class GameController : MonoBehaviour {
         new Vector3(1, 0, -1),
     };
 
+    private Coroutine showCubePlace;
+
     private void Start() {
         allCubesRb = allCubes.GetComponent<Rigidbody>();
-        StartCoroutine(ShowCubePlace());
+        showCubePlace = StartCoroutine(ShowCubePlace());
     }
 
     private void Update() {
-        if(Input.GetMouseButtonDown(0) || Input.touchCount > 0) {
+        if((Input.GetMouseButtonDown(0) || Input.touchCount > 0) && cubeToPlace != null) {
 #if !UNITY_EDITOR
             if (Input.GetTouch(0).phase != TouchPhase.Began)
                 return;
@@ -50,6 +54,12 @@ public class GameController : MonoBehaviour {
             allCubesRb.isKinematic = false;
 
             SpawnPositions();
+        }
+
+        if(!IsLose && allCubesRb.velocity.magnitude > 0.1f) {
+            Destroy(cubeToPlace.gameObject);
+            IsLose = true;
+            StopCoroutine(showCubePlace);
         }
     }
 
